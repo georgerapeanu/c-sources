0161b7bde0ed594a735a32f3342b8f730190bba0
georgerapeanu <alexandrurapeanu@yahoo.com>
2018-12-09 14:23:22 +0200

    testing,atention please

---
+++ APC_D_eval.bat
@@ -0,0 +1,7 @@
+@echo off
+:eval
+APC_D_gen.exe
+APC_D_brut.exe
+APC_D_sursa.exe
+APC_D_eval.exe
+goto eval
---
+++ APC_D_sursa.cpp
@@ -0,0 +1,136 @@
+#include <cstdio>
+#include <vector>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+ 	
+int MOD, Q;
+struct stack_node_t{
+	int w;
+	int v;
+	vector<long long> dp;
+	
+	stack_node_t(int w,int v){
+		this->w = w;
+		this->v = v;
+		vector<long long> base(MOD,-(1LL << 60));
+		base[0] = 0;
+		dp = base;
+		for(int i = 0;i < (int)base.size();i++){
+			dp[(i + w) % MOD] = max(dp[(i + w) % MOD],max(-(1LL << 60),base[i] + v));
+		}
+	}
+	
+	stack_node_t(int w,int v,vector<long long> &base){
+		this->w = w;
+		this->v = v;
+		dp = base;
+		for(int i = 0;i < (int)base.size();i++){
+			dp[(i + w) % MOD] = max(dp[(i + w) % MOD],max(-(1LL << 60),base[i] + v));
+		}
+	}
+};
+
+vector< stack_node_t > fst_stack;
+vector< stack_node_t > snd_stack;
+
+void add(int w,int v){
+	if(snd_stack.empty()){
+		snd_stack.push_back(stack_node_t(w,v));
+	}
+	else{
+		snd_stack.push_back(stack_node_t(w,v,snd_stack.back().dp));
+	}
+}
+
+void rem(){
+	if(fst_stack.empty()){
+		fst_stack.push_back(stack_node_t(snd_stack.back().w,snd_stack.back().v));
+		snd_stack.pop_back();
+		
+		while(!snd_stack.empty()){
+			fst_stack.push_back(stack_node_t(snd_stack.back().w,snd_stack.back().v,fst_stack.back().dp));
+			snd_stack.pop_back();
+		}
+	}
+	fst_stack.pop_back();
+}
+
+long long fi(int l,int r){
+	vector<long long> fst;
+	vector<long long> snd;
+	
+	if(fst_stack.empty()){
+		fst = vector<long long>(MOD,-(1LL << 60));
+		fst[0] = 0;
+	}
+	else{
+		fst = fst_stack.back().dp;
+	}
+	
+	if(snd_stack.empty()){
+		snd = vector<long long>(MOD,-(1LL << 60));
+		snd[0] = 0;
+	}
+	else{
+		snd = snd_stack.back().dp;
+	}
+	
+	deque<pair<long long,int> > max_dq;
+	
+	for(int i = r;i >= l;i--){
+		while(!max_dq.empty() && max_dq.back().first <= snd[i]){
+			max_dq.pop_back();
+		}
+		max_dq.push_back({snd[i],i});
+	}
+	
+	long long ans = -(1LL << 60);
+	
+	for(int i = 0;i < MOD;i++){
+		ans = max(ans,fst[i] + max_dq.front().first);
+		
+		if(max_dq.front().second == r){
+			max_dq.pop_front();
+		}
+		
+		r = (r == 0 ? MOD - 1:r - 1);
+		l = (l == 0 ? MOD - 1:l - 1);
+		
+		while(!max_dq.empty() && max_dq.back().first <= snd[l]){
+			max_dq.pop_back();
+		}
+		
+		max_dq.push_back({snd[l],l});
+	}
+	
+	return (ans < 0 ? -1:ans);
+}
+
+int main() {
+	
+	freopen("in","r",stdin);
+	freopen("out","w",stdout);
+
+	scanf("%d %d", &MOD, &Q);
+
+	for (int i = 0; i < Q; i++) {
+		int t, w, v, l, r;
+		scanf("%d %d %d %d %d", &t, &w, &v, &l, &r);
+
+		if (t == 1) {
+			add(w,v);
+		} 
+		
+		else {
+			rem();
+		}
+		
+		
+		long long ans = fi(l,r);
+		printf("%lld\n", ans);
+	}
+	
+	return 0;
+}
