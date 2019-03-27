397349bf2ad1d15bebc1f4ae2d64f8c50b30f911
georgerapeanu <alexandrurapeanu@yahoo.com>
2019-03-27 21:34:55 +0200

    ooasfmaposmfap

---
+++ sugar/denis/449E.cpp
@@ -0,0 +1,76 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+const int NMAX = 1e6;
+const int MOD = 1e9 + 7;
+
+int q;
+
+int f[NMAX + 5];
+int phi[NMAX + 5];
+int cost[NMAX + 5];
+
+int pre_n[NMAX + 5];
+int pre_l[NMAX + 5];
+int pre_s[NMAX + 5];
+
+int main(){
+
+    for(int x = 2;x <= NMAX;x++){
+        phi[x] += x - 1;
+
+        phi[x] %= MOD;
+
+        for(int y = 2 * x;y <= NMAX;y += x){
+            phi[y] -= phi[x];
+            if(phi[y] < 0){
+                phi[y] += MOD;
+            }
+        }
+    }
+
+    for(int x = 1;x <= NMAX;x++){
+        for(int k = 1;1LL * k * x <= NMAX;k++){
+            cost[k * x] += 1LL * k * phi[x] % MOD;
+            if(cost[k * x] >= MOD){
+                cost[k * x] -= MOD;
+            }
+        }
+        cost[x] += x;
+        cost[x] %= MOD;
+    }
+
+    for(int L = 1;L <= NMAX;L++){
+        f[L] = (1LL * L * (2 * L - 1) * (L - 1) / 3 - 1LL * L * L) % MOD;
+        f[L] += 2 * cost[L];
+        f[L] %= MOD;
+    }
+
+    for(int L = 1;L <= NMAX;L++){
+        pre_n[L] = (pre_n[L - 1] + f[L]) % MOD;
+        pre_l[L] = (pre_l[L - 1] + (1LL * f[L] * L % MOD)) % MOD;
+        pre_s[L] = (pre_s[L - 1] + (1LL * (1LL * f[L] * L % MOD) * L % MOD)) % MOD;
+    }
+
+    scanf("%d",&q);
+
+    while(q--){
+        int n,m;
+        scanf("%d %d",&n,&m);
+        if(n > m){
+            swap(n,m);
+        }
+        int ans = 0;
+        
+        ans = ((1LL * n * m + n + m + 1) % MOD) * pre_n[n] % MOD;
+        ans = (ans + MOD - ((1LL * (n + 1 + m + 1) % MOD) * pre_l[n] % MOD)) % MOD;
+        ans = (ans + pre_s[n]) % MOD;
+
+        printf("%d\n",ans); 
+    }
+
+
+    return 0;
+}
---
+++ sugar/denis/529A.cpp
@@ -0,0 +1,128 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+const int NMAX = 1e6;
+
+int n;
+char s[2 * NMAX + 5];
+
+int sa[NMAX + 5];
+int pos[2 * NMAX + 5];
+
+int pref_sum[NMAX + 5];
+
+int pref_min[NMAX + 5];
+int suff_min[NMAX + 5];
+
+int tmp[2 * NMAX + 5];
+
+int gap;
+
+bool cmp(int a,int b){
+    if(pos[a] != pos[b]){
+        return pos[a] < pos[b];
+    }
+    
+    a += gap;
+    b += gap;
+
+    return pos[a] < pos[b];
+}
+
+void build_sa(){
+    sort(sa + 1,sa + 1 + n,[&](int a,int b){return s[a] < s[b];});
+    for(int i = 1;i <= n;i++){
+        pos[sa[i]] = pos[sa[i - 1]] + (s[sa[i - 1]] != s[sa[i]]);
+        pos[sa[i] + n] = pos[sa[i]];
+    }
+
+    for(gap = 1;gap <= n;gap *= 2){
+        sort(sa + 1,sa + 1 + n,cmp);
+        tmp[sa[1]] = 1;
+        for(int i = 2;i <= n;i++){
+            tmp[sa[i]] = tmp[sa[i - 1]] + cmp(sa[i - 1],sa[i]);
+            tmp[sa[i] + n] = tmp[sa[i]];
+        }
+
+        for(int i = 1;i <= 2 * n;i++){
+            pos[i] = tmp[i];
+        }
+    }
+}
+
+int main(){
+
+    fgets(s + 1,NMAX + 5,stdin);
+
+    n = strlen(s + 1);
+    n -= (s[n] == '\n');
+
+    for(int i = n + 1;i <= 2 * n;i++){
+        s[i] = s[i - n];
+    }
+
+    for(int i = 1;i <= n;i++){
+        sa[i] = i;
+    }
+
+    for(int i = 1;i <= n;i++){
+        pref_sum[i] = (s[i] == '(' ? 1 : -1) + pref_sum[i - 1];
+        pref_min[i] = pref_sum[i];
+
+        if(i > 1){
+            pref_min[i] = min(pref_min[i - 1],pref_min[i]);
+        }
+    }
+
+    for(int i = n;i;i--){
+        suff_min[i] = pref_sum[i];
+
+        if(i < n){
+            suff_min[i] = min(suff_min[i + 1],suff_min[i]);
+        }
+    }
+
+    build_sa();
+
+    int cnt = pref_sum[n];
+
+    if(cnt > 0){
+        for(int i = 1;i <= n;i++){
+            if(suff_min[sa[i]] - pref_sum[sa[i] - 1] >= 0 && (pref_sum[n] - pref_sum[sa[i] - 1]) + pref_min[sa[i] - 1] >= 0){
+                for(int j = sa[i];j <= n;j++){
+                    fprintf(stdout,"%c",s[j]);
+                }
+                for(int j = 1;j < sa[i];j++){
+                    fprintf(stdout,"%c",s[j]);
+                }
+                for(int j = 1;j <= cnt;j++){
+                    fprintf(stdout,")");
+                }
+
+                return 0;
+            }
+        }
+    }
+    else{
+        for(int i = 1;i <= n;i++){
+            if(suff_min[sa[i]] - pref_sum[sa[i] - 1] >= cnt && (pref_sum[n] - pref_sum[sa[i] - 1]) + pref_min[sa[i] - 1] >= cnt){
+                for(int j = 1;j <= -cnt;j++){
+                    fprintf(stdout,"(");
+                }
+                for(int j = sa[i];j <= n;j++){
+                    fprintf(stdout,"%c",s[j]);
+                }
+                for(int j = 1;j < sa[i];j++){
+                    fprintf(stdout,"%c",s[j]);
+                }
+
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
